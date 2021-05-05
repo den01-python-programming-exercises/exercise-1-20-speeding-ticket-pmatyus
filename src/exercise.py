c6106710a4bc0892883ffc2049d84ffd4c985c1a
pmatyus <>
2021-05-05 14:57:35 +0000

    Exercise solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
     #write your code below this line
+    speed = int(input("Give speed:"))
+    if (speed > 70):
+      print("Speeding ticket!")
 
 if __name__ == '__main__':
     main()
